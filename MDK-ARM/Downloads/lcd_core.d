e75dd6071971ec32046200ead9acc623f4c6aec9
Yiyang Xiao <04242912@cumt.edu.cn>
2025-03-21 18:38:26 +0800

    first commit

---
+++ MDK-ARM/Downloads/lcd_core.d
@@ -0,0 +1,8 @@
+downloads\lcd_core.o: ..\Drivers\BSP\LCD_ST7789\lcd_core.c
+downloads\lcd_core.o: C:\Users\34118\AppData\Local\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
+downloads\lcd_core.o: C:\Users\34118\AppData\Local\Keil_v5\ARM\ARMCC\Bin\..\include\stdarg.h
+downloads\lcd_core.o: ..\Drivers\BSP\LCD_ST7789\lcd.h
+downloads\lcd_core.o: C:\Users\34118\AppData\Local\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+downloads\lcd_core.o: ..\Drivers\BSP\LCD_ST7789\lcd_port.h
+downloads\lcd_core.o: C:\Users\34118\AppData\Local\Keil_v5\ARM\ARMCC\Bin\..\include\stdbool.h
+downloads\lcd_core.o: ..\Drivers\BSP\LCD_ST7789\lcd_font.h
---
+++ MDK-ARM/Downloads/lcd_font.d
@@ -0,0 +1,3 @@
+downloads\lcd_font.o: ..\Drivers\BSP\LCD_ST7789\lcd_font.c
+downloads\lcd_font.o: ..\Drivers\BSP\LCD_ST7789\lcd_font.h
+downloads\lcd_font.o: C:\Users\34118\AppData\Local\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
