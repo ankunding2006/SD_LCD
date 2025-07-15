b7503614b9e86ba29a6927572a90881f8ed4ea71
Yiyang Xiao <04242912@cumt.edu.cn>
2025-07-15 15:35:37 +0800

    添加控制模块，包含线跟踪任务和开环转向控制函数，更新相关头文件和实现文件，重命名一些变量

--- Core/Src/LineDetect/gray_detection.h
+++ Core/Src/LineDetect/gray_detection.h
@@ -10,28 +10,27 @@ typedef uint32_t u32;
 
 typedef struct
 {
-	uint8_t bit1 : 1;
-	uint8_t bit2 : 1;
-	uint8_t bit3 : 1;
-	uint8_t bit4 : 1;
-	uint8_t bit5 : 1;
-	uint8_t bit6 : 1;
-	uint8_t bit7 : 1;
-	uint8_t bit8 : 1;
-	uint8_t bit9 : 1;
-	uint8_t bit10 : 1;
-	uint8_t bit11 : 1;
-	uint8_t bit12 : 1;
-	uint8_t bit13 : 1;
-	uint8_t bit14 : 1;
-	uint8_t bit15 : 1;
-	uint8_t bit16 : 1;
+    uint8_t bit1 : 1;
+    uint8_t bit2 : 1;
+    uint8_t bit3 : 1;
+    uint8_t bit4 : 1;
+    uint8_t bit5 : 1;
+    uint8_t bit6 : 1;
+    uint8_t bit7 : 1;
+    uint8_t bit8 : 1;
+    uint8_t bit9 : 1;
+    uint8_t bit10 : 1;
+    uint8_t bit11 : 1;
+    uint8_t bit12 : 1;
+    uint8_t bit13 : 1;
+    uint8_t bit14 : 1;
+    uint8_t bit15 : 1;
+    uint8_t bit16 : 1;
 } gray_flags;
 
-typedef union
-{
-	gray_flags gray;
-	uint16_t state;
+typedef union {
+    gray_flags gray;
+    uint16_t state;
 } _gray_state;
 
 void gpio_input_init(void);
@@ -43,7 +42,7 @@ void grey_sensorData_print(void);
 uint16_t pca9555_read_bit12(uint8_t slave_num);
 uint8_t i2c_CheckDevice(uint8_t _Address);
 extern int IsCertainGraySenorsAcrived(uint8_t start_graySensor, uint8_t end_graySensor);
-int Calculate_Turn_Pwm(void);
+int Calculate_Turn_Value(void);
 
 extern float gray_status[2], gray_status_backup[2][20];
 extern uint32_t gray_status_worse;
--- Core/Src/task/control.c
+++ Core/Src/task/control.c
@@ -1,1 +1,139 @@
+/**
+ * @file control.c
+ * @brief 控制任务
+ * @note:介绍一下硬件系统,我的车轮使用了两个伺服电机作为后轮驱动,电机的地址分别为0x01和0x02
+ * @version 0.1
+ * @date 2025-07-15
+ * @copyright Copyright (c) 2025
+ */
+#include "control.h"
+#include "main.h"
+#include "dma.h"
+#include "fatfs.h"
+#include "sdio.h"
+#include "spi.h"
+#include "tim.h"
+#include "usart.h"
+#include "gpio.h"
 
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include <stdlib.h>
+#include "my_menu.h"
+#include "delay.h"
+#include "usmart.h"
+#include "lfs_port.h"
+#include "Emm_V5.h"
+#include "car_config.h"
+#include "test.h"
+#include "gray_detection.h"
+
+#define CAR_BASE_SPEED 200 // 小车基础速度
+#define CAR_MAX_SPEED  400 // 小车最大速度
+/**
+ * @brief 设置两个电机的速度
+ * @param left_speed 左电机速度(单位:转/分钟)
+ * @param right_speed 右电机速度(单位:转/分钟)
+ * @note 速度都为正数的时候,小车向前走,速度都为负数的时候,小车向后走,速度一个为正一个为负的时候,小车向左转,速度一个为负一个为正的时候,小车向右转
+ * @param acc 加速度(0-255)
+ */
+void set_motor_speed(int16_t left_speed, int16_t right_speed, uint8_t acc)
+{
+    if (left_speed > 0)
+        Emm_V5_Vel_Control(0x01, 1, left_speed, acc, 0);
+    else
+        Emm_V5_Vel_Control(0x01, 0, -left_speed, acc, 0);
+    if (right_speed > 0)
+        Emm_V5_Vel_Control(0x02, 1, right_speed, acc, 0);
+    else
+        Emm_V5_Vel_Control(0x02, 0, -right_speed, acc, 0);
+}
+
+
+
+/**
+ * @brief 这个函数用来控制小车进行循迹
+ * @return 如果完成了循迹(即出现至少3个传感器都返回黑色)就返回1,否则返回0
+ */
+uint8_t line_following_task(void)
+{
+    // 1. 更新传感器数据
+    grey_sensor_Read();
+
+    // 2. 计算转向值
+    int turn_value = Calculate_Turn_Value();
+
+    // 3. 根据转向值调整电机速度
+    if (turn_value == INT16_MAX) {
+        // 检测到3个或更多传感器，任务完成
+        set_motor_speed(0, 0, 252); // 停止
+        return 1;                   // 完成循迹
+    } else if (turn_value == INT16_MIN) {
+        // 没有检测到线，可能脱轨了
+        // 策略：原地停车
+        set_motor_speed(0, 0, 252);
+    } else {
+        // 根据转向值调整左右轮速度
+        // turn_value > 0 表示线在左边，需要左转，左轮慢，右轮快
+        // turn_value < 0 表示线在右边，需要右转，左轮快，右轮慢
+        int16_t left_speed  = CAR_BASE_SPEED - turn_value;
+        int16_t right_speed = CAR_BASE_SPEED + turn_value;
+
+        // 对速度进行限幅
+        if (left_speed > CAR_MAX_SPEED)
+            left_speed = CAR_MAX_SPEED;
+        if (left_speed < 0)
+            left_speed = 0;
+        if (right_speed > CAR_MAX_SPEED)
+            right_speed = CAR_MAX_SPEED;
+        if (right_speed < 0)
+            right_speed = 0;
+
+        set_motor_speed(left_speed, right_speed, 252); // 使用一个默认加速度
+    }
+
+    return 0; // 循迹未完成
+}
+
+/**
+ * @brief Open loop steering control function
+ *
+ * @param angle 小车旋转的时间(ms),逆时针旋转为正,顺时针旋转为负. 0表示查询状态
+ * @param Rotate_Speed 转向转速值(单位:转/分钟)
+ * @param Rotate_Speed_Base 基础转速值(单位:转/分钟)
+ * @return uint8_t 1:完成 0:小车正在转向中
+ */
+uint8_t open_loop_steering_control(int16_t angle, int16_t Rotate_Speed, int16_t Rotate_Speed_Base)
+{
+    static uint32_t turn_end_time = 0;
+
+    // A non-zero angle indicates a new turn command.
+    if (angle != 0) {
+        int16_t left_speed, right_speed;
+
+        if (angle > 0) { // Counter-clockwise turn (left)
+            left_speed  = Rotate_Speed_Base - Rotate_Speed;
+            right_speed = Rotate_Speed_Base + Rotate_Speed;
+        } else { // Clockwise turn (right)
+            left_speed  = Rotate_Speed_Base + Rotate_Speed;
+            right_speed = Rotate_Speed_Base - Rotate_Speed;
+        }
+
+        set_motor_speed(left_speed, right_speed, 252); // Use a default acceleration
+        turn_end_time = HAL_GetTick() + abs(angle);
+        return 0; // Turn initiated
+    } else {
+        // angle is 0, check status of the current turn.
+        if (turn_end_time == 0) {
+            return 1; // No turn in progress, so it's "completed"
+        }
+
+        if (HAL_GetTick() >= turn_end_time) {
+            set_motor_speed(0, 0, 252); // Stop motors
+            turn_end_time = 0;          // Reset state
+            return 1;                   // Turn completed
+        }
+
+        return 0; // Still turning
+    }
+}
