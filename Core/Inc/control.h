75d750500af34c044ebdeaccfeb1bf24b5e0502a
Yiyang Xiao <04242912@cumt.edu.cn>
2025-07-16 09:02:41 +0800

    在控制头文件中添加了设置电机速度的函数声明

--- Core/Inc/control.h
+++ Core/Inc/control.h
@@ -5,5 +5,6 @@
 
 uint8_t line_following_task(void);
 uint8_t open_loop_steering_control(int16_t angle, int16_t PWM_Value, int16_t PWM_Base);
+void set_motor_speed(int16_t left_speed, int16_t right_speed, uint8_t acc);
 
 #endif // CONTROL_H
